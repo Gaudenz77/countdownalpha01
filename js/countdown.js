19c931ef9c2878b0fd95c77fdeb690e04880ab49
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2022-12-07 16:56:20 +0100

    damn

--- js/countdown.js
+++ js/countdown.js
@@ -14,7 +14,92 @@
         document.getElementById("demo").innerHTML = d; */
 
 
-
+// Pomodoro Example Timer
+var pomodoro = {
+  started : false,
+  minutes : 0,
+  seconds : 0,
+  fillerHeight : 0,
+  fillerIncrement : 0,
+  interval : null,
+  minutesDom : null,
+  secondsDom : null,
+  fillerDom : null,
+  init : function(){
+    var self = this;
+    this.minutesDom = document.querySelector('#minutes');
+    this.secondsDom = document.querySelector('#seconds');
+    this.fillerDom = document.querySelector('#filler');
+    this.interval = setInterval(function(){
+      self.intervalCallback.apply(self);
+    }, 1000);
+    document.querySelector('#work').onclick = function(){
+      self.startWork.apply(self);
+    };
+    document.querySelector('#shortBreak').onclick = function(){
+      self.startShortBreak.apply(self);
+    };
+    document.querySelector('#longBreak').onclick = function(){
+      self.startLongBreak.apply(self);
+    };
+    document.querySelector('#stop').onclick = function(){
+      self.stopTimer.apply(self);
+    };
+  },
+  resetVariables : function(mins, secs, started){
+    this.minutes = mins;
+    this.seconds = secs;
+    this.started = started;
+    this.fillerIncrement = 200/(this.minutes*60);
+    this.fillerHeight = 0;  
+  },
+  startWork: function() {
+    this.resetVariables(25, 0, true);
+  },
+  startShortBreak : function(){
+    this.resetVariables(5, 0, true);
+  },
+  startLongBreak : function(){
+    this.resetVariables(15, 0, true);
+  },
+  stopTimer : function(){
+    this.resetVariables(25, 0, false);
+    this.updateDom();
+  },
+  toDoubleDigit : function(num){
+    if(num < 10) {
+      return "0" + parseInt(num, 10);
+    }
+    return num;
+  },
+  updateDom : function(){
+    this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
+    this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
+    this.fillerHeight = this.fillerHeight + this.fillerIncrement;
+    this.fillerDom.style.height = this.fillerHeight + 'px';
+  },
+  intervalCallback : function(){
+    if(!this.started) return false;
+    if(this.seconds == 0) {
+      if(this.minutes == 0) {
+        this.timerComplete();
+        return;
+      }
+      this.seconds = 59;
+      this.minutes--;
+    } else {
+      this.seconds--;
+    }
+    this.updateDom();
+  },
+  timerComplete : function(){
+    this.started = false;
+    this.fillerHeight = 0;
+  }
+};
+window.onload = function(){
+pomodoro.init();
+};
 
 
         /* Example main function from Internet */
@@ -103,7 +188,7 @@ function stopInterval() {
 // Exercises
 
 
-let d;
+/* let d;
 
 d = new Date();
 d = new Date(111255454525);
@@ -131,7 +216,7 @@ console.log(d.toString());
 
 console.log(d.toISOString());
 
-console.log(d.toLocaleString());
+console.log(d.toLocaleString()); */
 // TEst getDate over
 
 // start try getelemntbyId
@@ -154,97 +239,56 @@ fieldFive.innerHTML = '<strong>00</strong';
 let fieldSix = document.getElementById('sixth-field');
 fieldSix.innerHTML = '<strong>00</strong';
 
+// Find the distance between now and the count down date
+var distance = countDownDate - now;
 
-// Pomodoro Example Timer
-var pomodoro = {
-    started : false,
-    minutes : 0,
-    seconds : 0,
-    fillerHeight : 0,
-    fillerIncrement : 0,
-    interval : null,
-    minutesDom : null,
-    secondsDom : null,
-    fillerDom : null,
-    init : function(){
-      var self = this;
-      this.minutesDom = document.querySelector('#minutes');
-      this.secondsDom = document.querySelector('#seconds');
-      this.fillerDom = document.querySelector('#filler');
-      this.interval = setInterval(function(){
-        self.intervalCallback.apply(self);
-      }, 1000);
-      document.querySelector('#work').onclick = function(){
-        self.startWork.apply(self);
-      };
-      document.querySelector('#shortBreak').onclick = function(){
-        self.startShortBreak.apply(self);
-      };
-      document.querySelector('#longBreak').onclick = function(){
-        self.startLongBreak.apply(self);
-      };
-      document.querySelector('#stop').onclick = function(){
-        self.stopTimer.apply(self);
-      };
-    },
-    resetVariables : function(mins, secs, started){
-      this.minutes = mins;
-      this.seconds = secs;
-      this.started = started;
-      this.fillerIncrement = 200/(this.minutes*60);
-      this.fillerHeight = 0;  
-    },
-    startWork: function() {
-      this.resetVariables(25, 0, true);
-    },
-    startShortBreak : function(){
-      this.resetVariables(5, 0, true);
-    },
-    startLongBreak : function(){
-      this.resetVariables(15, 0, true);
-    },
-    stopTimer : function(){
-      this.resetVariables(25, 0, false);
-      this.updateDom();
-    },
-    toDoubleDigit : function(num){
-      if(num < 10) {
-        return "0" + parseInt(num, 10);
-      }
-      return num;
-    },
-    updateDom : function(){
-      this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
-      this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
-      this.fillerHeight = this.fillerHeight + this.fillerIncrement;
-      this.fillerDom.style.height = this.fillerHeight + 'px';
-    },
-    intervalCallback : function(){
-      if(!this.started) return false;
-      if(this.seconds == 0) {
-        if(this.minutes == 0) {
-          this.timerComplete();
-          return;
-        }
-        this.seconds = 59;
-        this.minutes--;
-      } else {
-        this.seconds--;
-      }
-      this.updateDom();
-    },
-    timerComplete : function(){
-      this.started = false;
-      this.fillerHeight = 0;
-    }
-};
-window.onload = function(){
-  pomodoro.init();
-};
+// Time calculations for days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+// Display the result in the element with id="demo"
+document.getElementById("demo1").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s ";
+
+// If the count down is finished, write some text
+if (distance < 0) {
+  clearInterval(x);
+  document.getElementById("demo").innerHTML = "EXPIRED";
+}
+}, 1000);
+
+
+
 
 
 // Test get element by id inner HTML
 
-const myDiv = document.getElementById('my-div');
+/* const myDiv = document.getElementById('my-div');
 
 myDiv.innerHTML = '<b>this is the content</b>';
+
+function changeText(){
+  let mytext = document.getElementById('mytext');
+  mytext.innerHTML = 'that';
+} */
+
+/* const = MediaKeyStatusMap(months);
+
+myDate.getFullYear();
+console.log(myDate);
+ */
+var x = 0;
+
+const time = document.getElementById("demo");
+  setInterval(function () {
+    
+    time.innerHTML = x
+    x
+  }, 1000);
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 # Countdown Timer Project:<br>Create a count down timer like web-devs do!
 ### Readme besser (explain project/how to install/how to use(contribute/license)) banner logo title/badge/
 ---
+## How to fullfill assignment
+My momentary knowledge and usage of Javascript itself led me to the decision, to try at least reach the target gola to try my best in imitating various templates and look how impolement found knwledge into my project. I dont see myself actually programming such coding in Javascript myself and therefor honestly declare my actions.
