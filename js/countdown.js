d9fe56016fdfcbd170a79bf3ebf058b7965a3938
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2022-12-08 11:48:21 +0100

    final sketch functinal files

--- js/countdown.js
+++ js/countdown.js
@@ -1,65 +1,175 @@
-var days;
-var hours;
-var minutes;
-var seconds;
-
 var count; 
-var maxCountMin = 60;
-var intervalIdMin;
-
-
-function setDisplay2(minuten) {  // kann String od Zahl sein
-    document.getElementById("display2").innerText = minuten;}
-
-
-function startCountdownMin() {
-    stopCountdownMin(); // falls es schomn eine Timer gibt: zuerst stoppen
- 
-    setDisplay2(maxCountMin); // Unterfunktion, 
-    minutes = maxCountMin;   // Counter in Anfangszustand
-
-    // Verwende die Referenz (namen) OHNE KLAMMER, da die Funktion nur geholt(ref) und untenals ausführung gesetzt wird
- 
-    intervalIdMin = setInterval(updateCounterMin, 1000);
-}
-
-function stopCountdownMin() {
-    // javascript-eigene funktion
-
-    clearInterval(intervalIdMin);
-}
-
-function updateCounterMin() {
-
-
-    minutes = minutes - 1; // kurzform count--;
-
-    // if done then stop updating
-    if(minutes === 0) {
-        //countdown fertig
-        setDisplay2('Go!');
-        stopCountdownMin();
-    }
-    else {
-        // zeigt aktuellen Count
-        setDisplay2(minutes);
-    }
-    
-}
-
-const thisDate = new Date();
-console.log(thisDate);
-
+var maxCount = getTimeValues(); /* sekunden inputfield */;
+var intervalId;
 
 function getTimeValues() {
-  var  hours = parseInt(document.getElementById("stunden").value);
-  var  minutes = parseInt(document.getElementById("minuten").value);
-  var  seconds = parseInt(document.getElementById("sekunden").value);
-
+  let  hours = parseInt(document.getElementById("stunden").value);
+  let  minutes = parseInt(document.getElementById("minuten").value);
+  let  seconds = parseInt(document.getElementById("sekunden").value);
+  let total = (hours * 60 * 60) + (minutes * 60) + seconds
 
   console.log(seconds, minutes, hours);
-  return false;
+  console.log(total);
+  return total;   
 }
+
+
+// my layout //
+let fieldOne = document.getElementById('first-field');
+fieldOne.innerHTML = '<strong>22</strong';
+
+let fieldTwo = document.getElementById('second-field');
+fieldTwo.innerHTML = '<strong>00</strong';
+
+let fieldThree = document.getElementById('third-field');
+fieldThree.innerHTML = '<strong>00</strong';
+
+function setDisplay(info) {  // kann String od Zahl sein
+    document.getElementById("display").innerText = info;
+  }
+
+function startTimer() {
+  stopTimer(); // falls es schomn eine Timer gibt: zuerst stoppen
+  setDisplay(maxCount); // Unterfunktion, 
+  count = maxCount;   // Counter in Anfangszustand
+  // Verwende die Referenz (namen) OHNE KLAMMER, da die Funktion nur geholt(ref) und untenals ausführung gesetzt wird
+  intervalId = setInterval(updateCounter, 1000); // jede sekunde zähler aktualisiert
+
+}
+
+function stopTimer() {
+  // javascript-eigene funktion
+  clearInterval(intervalId);
+
+}
+
+
+function updateCounter() {
+/* Stunden= count/3600
+  Minuten= count/60 */
+  console.log('count =', count);
+  count = count - 1; // kurzform count--;
+  setDisplay(count);
+
+  let hours = Math.floor(count / 3600);
+  let minutes = Math.floor((count % 3600) / 60);
+  let seconds = count % 60;
+
+console.log(hours, minutes, seconds);
+
+  setHours(hours);
+  setMinutes(minutes);
+  setSeconds(seconds);
+  
+  function setHours(h){  // kann String od Zahl sein
+    document.getElementById("Hours").innerText = h;
+  }
+  function setMinutes(m){  // kann String od Zahl sein
+    document.getElementById("Minutes").innerText = m;
+  }
+  function setSeconds(s){  // kann String od Zahl sein
+    document.getElementById("Seconds").innerText = s;
+  }
+
+
+
+  // if done then stop updating
+  if(count <= 0) {
+      //countdown fertig
+      setDisplay('Go!');
+      stopCountdown();
+  }
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const comingYear = document.querySelector(".coming-year");
+const birthDate = prompt("Enter birth date (1-31):");
+const birthMonth = prompt("Enter birth month (1-12):");
+const today = new Date();
+var nextYear;
+if (today.getMonth() + 1 > birthMonth || (today.getMonth() + 1 == birthMonth && today.getDate() + 1 > birthDate)) {
+  nextYear = new Date().getFullYear() + 1;
+}
+else {
+  nextYear = new Date().getFullYear();
+}
+comingYear.innerHTML = nextYear;
+function countdown() {
+  const remaining = new Date(`\${nextYear}-\${birthMonth}-\${birthDate} 00:00:00`) - new Date();
+  const days = Math.floor(remaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(remaining / (1000 * 60 * 60)) % 24;
+  const minutes = Math.floor(remaining / (1000 * 60)) % 60;
+  const seconds = Math.floor(remaining / 1000) % 60;
+  document.querySelector(".days").innerHTML = days;
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML = minutes;
+  document.querySelector(".seconds").innerHTML = seconds;
+}
+countdown()
+setInterval(countdown, 1000); */
+
+
+
+/* let myTest = document.getElementById('demo');
+myTest.innerHTML = '<strong>Test</strong';
+
+const myName = "Gaudenz";
+
+console.log(myName);
+
+const thisDate = new Date();
+console.log(thisDate); */
+
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 8, 2022 15:30:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+// Get today's date and time
+var now = new Date().getTime();
+
+// Find the distance between now and the count down date
+var distance = countDownDate - now;
+
+// Time calculations for days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+// Display the result in the element with id="demo"
+/* document.getElementById("demo1").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s "; */
+
+// If the count down is finished, write some text
+if (distance < 0) {
+  clearInterval(x);
+  document.getElementById("demo").innerHTML = "EXPIRED";
+}
+}, 1000);
+
 
 //  I need:
 const timeNeeded = new Date();
@@ -95,109 +205,6 @@ countdownMilliSec = startTimer (input-no. input-field)
 
 
 */
-// my layout //
-let fieldOne = document.getElementById('first-field');
-fieldOne.innerHTML = '<strong>00</strong';
-
-let fieldTwo = document.getElementById('second-field');
-fieldTwo.innerHTML = '<strong>00</strong';
-
-let fieldThree = document.getElementById('third-field');
-fieldThree.innerHTML = '<strong>00</strong';
-
-var count; 
-var maxCount = 60;
-var intervalId;
-
-
-function setDisplay(info) {  // kann String od Zahl sein
-    document.getElementById("display").innerText = info;}
-
-function startTimer() {
-  stopTimer(); // falls es schomn eine Timer gibt: zuerst stoppen
-  setDisplay(maxCount); // Unterfunktion, 
-  count = maxCount;   // Counter in Anfangszustand
-  // Verwende die Referenz (namen) OHNE KLAMMER, da die Funktion nur geholt(ref) und untenals ausführung gesetzt wird
-  intervalIdSec = setInterval(updateCounter, 1000); // jede sekunde zähler aktualisiert
-
-}
-
-function stopTimer() {
-  // javascript-eigene funktion
-  clearInterval(intervalId);
-
-}
-
-
-// Set the date we're counting down to
-var countDownDate = new Date("Dec 8, 2022 15:30:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-// Get today's date and time
-var now = new Date().getTime();
-
-// Find the distance between now and the count down date
-var distance = countDownDate - now;
-
-// Time calculations for days, hours, minutes and seconds
-var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-// Display the result in the element with id="demo"
-document.getElementById("demo1").innerHTML = days + "d " + hours + "h "
-+ minutes + "m " + seconds + "s ";
-
-// If the count down is finished, write some text
-if (distance < 0) {
-  clearInterval(x);
-  document.getElementById("demo").innerHTML = "EXPIRED";
-}
-}, 1000);
-
-
-/* let myTest = document.getElementById('demo');
-myTest.innerHTML = '<strong>Test</strong';
-
-const myName = "Gaudenz";
-
-console.log(myName);
-
-const thisDate = new Date();
-console.log(thisDate); */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
